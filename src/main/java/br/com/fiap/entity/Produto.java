ea4bcc2e96202630d1c212b86928ba1f0abf5093
Igor Guilherme <ifgandrade@outlook.com>
2020-08-10 22:27:10 -0300

    many to many produtos

--- src/main/java/br/com/fiap/entity/Produto.java
+++ src/main/java/br/com/fiap/entity/Produto.java
@@ -1,10 +1,18 @@
 package br.com.fiap.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -12,8 +20,8 @@ import javax.persistence.Table;
 public class Produto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "produtoId")
-	private Long produtoId;
+	@Column(name = "ID")
+	private int id;
 	@Column(name = "nome", length = 45)
 	private String nome;
 	@Column(name = "quantidade")
@@ -21,12 +29,19 @@ public class Produto {
 	@Column(name = "valor")
 	private double valor;
 
-	public Long getProdutoId() {
-		return produtoId;
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "PRODUTO_PEDIDO", catalog = "dbecommerce", joinColumns = {
+			@JoinColumn(name = "PRODUTO_ID", nullable = false, updatable = false) }, inverseJoinColumns = {
+					@JoinColumn(name = "PEDIDO_ID", nullable = false, updatable = false) })
+
+	private Set<Pedido> pedidos = new HashSet<>();
+
+	public int getProdutoId() {
+		return id;
 	}
 
-	public void setProdutoId(Long produtoId) {
-		this.produtoId = produtoId;
+	public void setProdutoId(int id) {
+		this.id = id;
 	}
 
 	public String getNome() {
@@ -51,6 +66,14 @@ public class Produto {
 
 	public void setValor(double valor) {
 		this.valor = valor;
+	}
+
+	public Set<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setTarefas(Set<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 }
