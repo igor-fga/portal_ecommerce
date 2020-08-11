c9e4300c5d0e859a59986b81e793ec4bfd5762d0
Diego <Diego@DESKTOP-94RMNI6>
2020-08-10 22:39:26 -0300

    md3

--- src/main/java/br/com/fiap/entity/Pedido.java
+++ src/main/java/br/com/fiap/entity/Pedido.java
@@ -11,7 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
