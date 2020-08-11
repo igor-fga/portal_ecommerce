bbc9b765416c8c1413059e2a79876b45cdbe445c
Diego <Diego@DESKTOP-94RMNI6>
2020-08-10 22:13:03 -0300

    alteracao entidade

--- src/main/java/br/com/fiap/entity/Pedido.java
+++ src/main/java/br/com/fiap/entity/Pedido.java
@@ -1,15 +1,58 @@
 package br.com.fiap.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "pedidos")
+@Table(name = "pedido")
 public class Pedido {
 	
 	@Id
-    public Integer id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+    private Integer id;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "cliente")
+	private Set<Cliente> cliente = new HashSet<>();
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "produto")
+	private Set<Produto> produto = new HashSet<>();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Set<Cliente> getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Set<Cliente> cliente) {
+		this.cliente = cliente;
+	}
+
+	public Set<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Set<Produto> produto) {
+		this.produto = produto;
+	}
+
+	
     
 
 }
