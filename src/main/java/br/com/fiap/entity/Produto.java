94833123a801b4860ac716361d21647bbdaf3a2f
Igor Guilherme <ifgandrade@outlook.com>
2020-08-10 21:38:54 -0300

    criado entidade produtos

--- src/main/java/br/com/fiap/entity/Produto.java
+++ src/main/java/br/com/fiap/entity/Produto.java
@@ -1,5 +1,56 @@
 package br.com.fiap.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "produtoId")
+	private int produtoId;
+	@Column(name = "nome", length = 45)
+	private String nome;
+	@Column(name = "quantidade")
+	private int quantidade;
+	@Column(name = "valor")
+	private double valor;
+
+	public int getProdutoId() {
+		return produtoId;
+	}
+
+	public void setProdutoId(int produtoId) {
+		this.produtoId = produtoId;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
 
 }
