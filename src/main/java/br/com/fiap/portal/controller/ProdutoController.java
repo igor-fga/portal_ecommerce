f32c73909ba735a1f584d124bcafe7624166301f
Igor Guilherme <ifgandrade@outlook.com>
2020-08-13 01:32:49 -0300

    Iniciando a API

---
+++ src/main/java/br/com/fiap/portal/controller/ProdutoController.java
@@ -0,0 +1,31 @@
+package br.com.fiap.portal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.portal.entity.Produto;
+import br.com.fiap.portal.repository.ProdutoRepository;
+
+@RestController
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository _produtoRepository;
+
+	@RequestMapping(value = "/produto", method = RequestMethod.GET)
+	public List<Produto> Get() {
+		return _produtoRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/produto", method =  RequestMethod.POST)
+    public Produto Post(@RequestBody Produto produto)
+    {
+        return _produtoRepository.save(produto);
+    }
+	
+}
